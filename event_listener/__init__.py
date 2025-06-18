b4bd1500eb5906c2b80e478abdb72fb75cf4bab1
yashwanth140 <ykanta@my.bridgeport.edu>
2025-06-18 16:46:47 -0400

    updated file _init_.py

--- event_listener/__init__.py
+++ event_listener/__init__.py
@@ -1,7 +1,13 @@
 import logging
+import json
 import azure.functions as func
 
-def main(events: func.EventHubEvent):
-    for event in events:
-        message = event.get_body().decode("utf-8")
-        logging.info("📨 Event received: %s", message)
+def main(event: func.EventHubEvent, outputBlob: func.Out[str]) -> None:
+    try:
+        message = event.get_body().decode('utf-8')
+        logging.info(f"✅ Event received: {message}")
+        data = json.loads(message)
+        outputBlob.set(json.dumps(data, indent=2))
+        logging.info("📤 Data written to Blob Storage.")
+    except Exception as e:
+        logging.error(f"❌ Error: {e}")
